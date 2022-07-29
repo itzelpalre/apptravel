4207ff73a55a2335d9956658fd21be003de52f0b
itzelpalre <itzelpalmareyes@gmail.com>
2022-07-29 11:24:01 -0500

    Updates

---
+++ precache-manifest.fa0a775f4ea446fe0e601f83f8c484e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac706a4a55a500dd9186",
+    "url": "/apptravel/static/css/main.18ed8d4c.chunk.css"
+  },
+  {
+    "revision": "ac706a4a55a500dd9186",
+    "url": "/apptravel/static/js/main.ac706a4a.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/apptravel/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "6a369ad33682b49af0e0",
+    "url": "/apptravel/static/css/2.91cf5aa7.chunk.css"
+  },
+  {
+    "revision": "6a369ad33682b49af0e0",
+    "url": "/apptravel/static/js/2.6a369ad3.chunk.js"
+  },
+  {
+    "revision": "c038abf046f35d660a88",
+    "url": "/apptravel/static/js/runtime~main.c038abf0.js"
+  },
+  {
+    "revision": "206167307a333cfc907ee71263db4225",
+    "url": "/apptravel/static/media/img01.20616730.jpg"
+  },
+  {
+    "revision": "8ef638f2f3e406a76d1fac092fe21620",
+    "url": "/apptravel/static/media/img02.8ef638f2.jpg"
+  },
+  {
+    "revision": "8cd632883d55b864fb56053e515ef6b1",
+    "url": "/apptravel/static/media/montserrat-regular-webfont.8cd63288.woff2"
+  },
+  {
+    "revision": "8bc9746726b68537d738e4bc361fc988",
+    "url": "/apptravel/index.html"
+  }
+];
